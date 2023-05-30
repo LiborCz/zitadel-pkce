60f4e1733ea557c16a35383152db5baae00078f5
Libor Pilný <libor.pilny@avast.com>
2023-05-30 15:30:15 +0200

    chore: initial commit

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,11 +1,11 @@
 const authConfig = {
-    authority: 'https://some_text.zitadel.cloud/', //Replace with your issuer URL
-    client_id: 'ABC123@Project', //Replace with your client id
+    authority: 'https://eci-test-imhndk.zitadel.cloud', //Replace with your issuer URL
+    client_id: '216348643593093377@eci-test', //Replace with your client id
     redirect_uri: 'http://localhost:3000/callback',
     response_type: 'code',
     scope: 'openid profile email',
     post_logout_redirect_uri: 'http://localhost:3000/',
-    userinfo_endpoint: 'https://instance-some_text.zitadel.cloud/oidc/v1/userinfo', //Replace with your user-info endpoint
+    userinfo_endpoint: 'https://eci-test-imhndk.zitadel.cloud/oidc/v1/userinfo', //Replace with your user-info endpoint
     response_mode: 'query',
     code_challenge_method: 'S256',
   };
